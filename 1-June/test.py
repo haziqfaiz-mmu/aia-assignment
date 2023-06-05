55d7a82cd215d678a2f9653078fa46f62a09831c
haziqfaiz-mmu <1201302740@student.mmu.edu.my>
2023-06-05 23:55:29 +0800

    tengah clean

---
+++ 1-June/test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import re
+
+f=open("ds_test_log.txt","r")
+
+#for line in f:
+#    line = line.replace("\nruser","ruser").replace("\nrhost","rhost")
+
+for line in f:
+    print(line)
